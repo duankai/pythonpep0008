c2a734b4640ed3b8b44e85ccc01f41fe4fcae172
Kai Duan <duankai@yeah.net>
2016-03-02 17:45:35 +0800

    pep2

---
+++ pep2.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+import os
+import nltk as nk
+
+def getNumberAndPrice(number, price):
+    if price > 120:
+        print("price is too high")
+    else:
+        print(number, price)
+
+getNumberAndPrice(170, 12)
+
+def spam():
+    pass
+    
+spam(ham[1], {eggs: 2})
